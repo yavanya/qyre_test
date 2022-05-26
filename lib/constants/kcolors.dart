f36610536d4f0bc69913a2831c320f9fa5bed6be
Ivan Malygin <yavanya@gmail.com>
2022-05-26 23:05:12 +0400

    colors and textstyles added

---
+++ lib/constants/kcolors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class KColors {
+  static const black = Color(0xff111111);
+  static const grey = Color(0xff444444);
+  static const lightGrey = Color(0xffF0F2F5);
+  static const lightBlue = Color(0xff87C6F5);
+  static const red = Color(0xffEC4E27);
+  static const fontLightGrey = Color(0xff9C9C9C);
+  static const fontDarkGrey = Color(0xff656565);
+  static const startGradBlue = Color(0xff3465C3);
+  static const endGradBlue = Color(0xff5785DE);
+  static const startGradRed = Color(0xffEC4E27);
+  static const endGradRed = Color(0xffF47E61);
+  static const startGradPurple = Color(0xff6B34C3);
+  static const endGradPurple = Color(0xff8E5EDB);
+}
---
+++ lib/constants/ktext_styles.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:qyre_test/constants/kcolors.dart';
+
+class KTextStyles {
+  static const _family = 'Matter';
+
+  static const appBar = TextStyle(
+    fontFamily: _family,
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: KColors.grey,
+  );
+
+  static const title16 = TextStyle(
+    fontFamily: _family,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: KColors.black,
+  );
+
+  static const title14 = TextStyle(
+    fontFamily: _family,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: KColors.black,
+  );
+
+  static const subTitle14 = TextStyle(
+    fontFamily: _family,
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    color: KColors.black,
+  );
+
+  static const subTitle12 = TextStyle(
+    fontFamily: _family,
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: KColors.black,
+  );
+
+  static const subTitle10 = TextStyle(
+    fontFamily: _family,
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    color: KColors.black,
+  );
+
+  static const paragraph = TextStyle(
+    fontFamily: _family,
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    color: KColors.black,
+  );
+}
